826d7bc2fe85c0155249e9e7aa15ba11cec2e6c2
Ethan Peterson <cap2010.ep@gmail.com>
2018-01-17 22:21:41 -0700

    Add basic comments describing current activity.

--- In-Labs/1/etch-a-sketch.py
+++ In-Labs/1/etch-a-sketch.py
@@ -3,9 +3,9 @@ import random
 
 window = turtle.Screen()
 
-square = turtle.Turtle()
-square.speed(0)
-square.hideturtle()
+square = turtle.Turtle() # define the turtle responsible for drawing the square
+square.speed(0) # make the turtle draw at light-speed
+square.hideturtle() # make the turtle itself invisible
 
 square.up()
 square.goto(-200, 200)
@@ -19,7 +19,7 @@ square.up()
 square.goto(-205, 205)
 square.write("Change Color")
 
-pencil = turtle.Turtle()
+pencil = turtle.Turtle() # define the turtle that will be the color-changing pencil
 pencil.shape("circle")
 
 def drawing_controls(x, y):
@@ -27,7 +27,7 @@ def drawing_controls(x, y):
         red = random.random()
         green = random.random()
         blue = random.random()
-        pencil.color(red, green, blue)       
+        pencil.color(red, green, blue)
 
 window.onclick(drawing_controls)
 
