3be22bdc46896ae3d6d0d4a4095cce2120e21f73
Ethan Peterson <cap2010.ep@gmail.com>
2018-01-21 23:09:59 -0700

    Add ability to calculate tax for 2018.

--- In-Labs/2/lab2.py
+++ In-Labs/2/lab2.py
@@ -1,25 +1,23 @@
-# ---------------------------------------
-# CSCI 127, Joy and Beauty of Data
-# Lab 2: Tax Calculator
-# Your Name
-# Date: January 25, 2018
-# ---------------------------------------
-# Calculate the amount of tax owed by a
-# single taxpayer in 2018. 
-# ---------------------------------------
-
-
-# The missing Python function goes here.
-
-
-# ---------------------------------------
+def calculate_tax(income):
+    if income > 500000:
+        return income * .37
+    elif income > 200000:
+        return income * .35
+    elif income > 157500:
+        return income * .32
+    elif income > 82500:
+        return income * .24
+    elif income > 38700:
+        return income * .22
+    elif income > 9525:
+        return income * .12
+    else:
+        return income * .1
 
 def process(income):
-    print("In tax year 2017, you earned ${:.2f}".format(income))
+    print("In tax year 2018, you earned ${:.2f}".format(income))
     tax_owed = calculate_tax(income)
     print("Your tax bill is ${:.2f}\n".format(tax_owed))
-
-#---------------------------------------
 
 def main():
     process(500)
@@ -30,7 +28,5 @@ def main():
     process(250000)
     process(500000)
     process(750000)
-
-# ---------------------------------------
 
 main()
