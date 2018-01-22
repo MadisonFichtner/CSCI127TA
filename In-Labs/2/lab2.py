5a0faffa67643ef5aff32a86d2e4907d47bdd294
Ethan Peterson <cap2010.ep@gmail.com>
2018-01-22 12:24:40 -0700

    Calculate tax owed using marginal rates.

--- In-Labs/2/lab2.py
+++ In-Labs/2/lab2.py
@@ -1,16 +1,17 @@
 def calculate_tax(income):
+    ''' Calculate tax owed based on marginal tax rates for the given income. '''
     if income > 500000:
-        return income * .37
+        return (income - 500000) * .37 + calculate_tax(500000)
     elif income > 200000:
-        return income * .35
+        return (income - 200000) * .35 + calculate_tax(200000)
     elif income > 157500:
-        return income * .32
+        return (income - 157500) * .32 + calculate_tax(157500)
     elif income > 82500:
-        return income * .24
+        return (income - 82500) * .24 + calculate_tax(82500)
     elif income > 38700:
-        return income * .22
-    elif income > 9525:
-        return income * .12
+        return (income - 38700) * .22 + calculate_tax(38700)
+    if income > 9525:
+        return (income - 9525) * .12 + calculate_tax(9525)
     else:
         return income * .1
 
