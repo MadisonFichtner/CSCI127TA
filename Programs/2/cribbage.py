39bf0bf34b0cbfed864f3a2e1ecb733d3e6c220c
Ethan Peterson <cap2010.ep@gmail.com>
2018-01-31 15:50:15 -0700

    Add ability to print cribbage hand.

--- Programs/2/cribbage.py
+++ Programs/2/cribbage.py
@@ -1,26 +1,27 @@
+def evaluate_hand(hand):
+    points = 0
+    print("Points scored:",points,"\n")
 
-# --------------------------------------
-# Do not change anything below this line
-# --------------------------------------
+def print_hand(hand):
+    print("Cribbage Hand\n-------------")
+    i = 1
+    for card in hand:
+        print("Card",i,":",card[0],"of",card[1].title())
+        i += 1
 
 def process_hands(cribbage_input, cards_in_hand):
-
     for hand in cribbage_input:
         hand = hand.split()
         hand_as_list = []
-        for i in range(cards_in_hand):
+        for _ in range(cards_in_hand):
             hand_as_list.append([int(hand[0]), hand[1]])
             hand = hand[2:]
         print_hand(hand_as_list)
         evaluate_hand(hand_as_list)
 
-# --------------------------------------
-
 def main():
     poker_input = open("cribbage.txt", "r")
     process_hands(poker_input, 3)
     poker_input.close()
-
-# --------------------------------------
 
 main()
