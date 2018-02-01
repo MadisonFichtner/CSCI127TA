5c8bb1d431f0ab18b4b561b6b8f7b445e3935b9c
Ethan Peterson <cap2010.ep@gmail.com>
2018-01-31 23:31:04 -0700

    Add ability to evaluate the point-value of a hand.

--- Programs/2/cribbage.py
+++ Programs/2/cribbage.py
@@ -1,6 +1,37 @@
-def evaluate_hand(hand):
-    points = 0
-    print("Points scored:",points,"\n")
+def score_3_of_a_kind(cvals):
+    if cvals[0] == cvals[1] and cvals[0] == cvals[2]: # 3 of a kind
+        return 6
+    return 0
+
+def score_pair(cvals):
+    if cvals[0] == cvals[1] or cvals[1] == cvals[2]: # pair
+        return 2
+    return 0
+
+def score_sequence(cvals):
+    if cvals[1] == (cvals[0] + 1) and cvals[2] == (cvals[1] + 1): # sequence
+        return 3
+    return 0
+
+def score_fifteen(cvals):
+    cfif = 0 # the number of 15s that can be made
+    if (cvals[0] + cvals[1] + cvals[2]) == 15:
+        cfif += 1
+    if cvals[0] + cvals[1] == 15:
+        cfif += 1
+    if cvals[1] + cvals[2] == 15:
+        cfif += 1
+    if cvals[0] + cvals[2] == 15:
+        cfif += 1
+    return 2 * cfif # award points for each 15
+
+def evaluate_hand(hand_as_list):
+    cvals = [] # keep track of the values of the cards
+    for card in hand_as_list:
+        cvals.append(card[0]) # keep track of the card's value
+    cvals.sort() # arrange the values in numerical order
+    
+    print("Points scored:",max(score_3_of_a_kind(cvals),score_pair(cvals))+score_sequence(cvals)+score_fifteen(cvals),"\n")
 
 def print_hand(hand):
     print("Cribbage Hand\n-------------")
