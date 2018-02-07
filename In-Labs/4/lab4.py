dfc4767026647babdc5703a2e6fc92128c0e55b7
Ethan Peterson <cap2010.ep@gmail.com>
2018-02-07 12:34:15 -0700

    Determine initial number of w-t-l

--- In-Labs/4/lab4.py
+++ In-Labs/4/lab4.py
@@ -1,29 +1,26 @@
-# --------------------------------------
-# CSCI 127, Lab 4
-# February 8, 2018
-# Your Name
-# --------------------------------------
+def possible_scores(numPlayed, ptsEarned):
+    # determine initial number of w-t-l
+    wins = ptsEarned // 3
+    ties = ptsEarned - wins * 3
+    losses = numPlayed - wins - ties
+
+    print(wins,"-",ties,"-",losses)
 
 def process_season(season, games_played, points_earned):
     print("Season: " + str(season) + ", Games Played: " + str(games_played) +
           ", Points earned: " + str(points_earned))
     print("Possible Win-Tie-Loss Records")
     print("-----------------------------")
-    pass
+    possible_scores(games_played, points_earned)
     print()
 
-# --------------------------------------
-
 def process_seasons(seasons):
-    pass
-
-# --------------------------------------
+    for i in range(len(seasons)):
+        process_season(i+1, seasons[i][0], seasons[i][1])
 
 def main():
     # format of list: [[season-1-games, season-1-points], [season-2-games, season-2-points], etc.]
     soccer_seasons = [[1, 3], [1, 1], [1, 0], [20, 30]]
     process_seasons(soccer_seasons)
-
-# --------------------------------------
 
 main()
