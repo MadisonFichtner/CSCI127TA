42a1a703832426cb804a16659a1be00ce339fa13
Ethan Peterson <cap2010.ep@gmail.com>
2018-03-05 21:57:58 -0700

    Comment Contact class.

--- In-Labs/8/lab8.py
+++ In-Labs/8/lab8.py
@@ -1,26 +1,32 @@
+# Contact represents a contact entity in a phone book
 class Contact:
     def __init__(self, first, last, phone):
-        self.first = first
-        self.last = last
-        self.phone = phone
-        self.title = ''
+        self.first = first # first name
+        self.last = last # last name
+        self.phone = phone # full phone number: area_code-first_3_digits-last_4_digits
+        self.title = '' # contact's title
 
+    # get_area_code returns the area code of the phone number
     def get_area_code(self):
         return self.phone[0:3]
 
+    # get_cell_number returns the full phone number for the contact
     def get_cell_number(self):
         return self.phone
 
+    # print_entry pretty prints the contact's information
     def print_entry(self):
         name = ''
-        if self.title != '':
+        if self.title != '': # only accomodate pretty printing of title if there is one
             name += self.title + ' '
         name += self.first + ' ' + self.last
         print((name).ljust(24), self.phone)
 
+    # set_first_name modifies the contact's first name
     def set_first_name(self, first):
         self.first = first
 
+    # set_title modifies the contact's title
     def set_title(self, title):
         self.title = title
 
